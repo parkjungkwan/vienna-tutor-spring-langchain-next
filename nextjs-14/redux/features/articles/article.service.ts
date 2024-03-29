24fe27e1777efc7623a16dab03b449d9ecdcf1ff
parkjungkwan <pakjkwan@gmail.com>
2024-03-29 18:44:17 +0900

    .

--- nextjs-14/redux/features/articles/article.service.ts
+++ nextjs-14/redux/features/articles/article.service.ts
@@ -9,9 +9,12 @@ export const getAllArticles: any = createAsyncThunk(
     'articles/getAllArticles',
     async (page: number)=>{
         console.log('getArticles page : '+ page)
-        const {message, result}:any = await  getAllArticlesAPI(1);
-        console.log('----- API 를 사용한 경우 -----')
-        console.log('message : '+ message)
-        console.log(JSON.stringify(result))
+        const data:any = await  getAllArticlesAPI(1);
+
+        const {message, result}:any = data
+        // console.log('----- API 를 사용한 경우 -----')
+        // console.log('message : '+ message)
+        // console.log(JSON.stringify(result))
+        return data
     }
 )
--- nextjs-14/redux/reducer.ts
+++ nextjs-14/redux/reducer.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
 import countReducer from "@/redux/features/counter/counter.slice";
+import articleReducer from "@/redux/features/articles/article.slice";
 import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
 const createNoopStorage = () => {
@@ -27,9 +28,19 @@ const countPersistConfig = {
   storage,
   whitelist: ["countState"],
 };
+const articlePersistConfig = {
+  key: "article",
+  storage,
+  whitelist: ["articleState"],
+};
 
-const persistedReducer = persistReducer(countPersistConfig, countReducer);
+
+const persistedCountReducer = persistReducer(countPersistConfig, countReducer);
+const persistedArticleReducer = persistReducer(articlePersistConfig, articleReducer);
 
 export const rootReducer = combineReducers({
-  count: persistedReducer,
+  count: persistedCountReducer,
+  article: persistedArticleReducer
 });
+
+
